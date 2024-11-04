d5582757c7067e825975660c6b979e64a4fe282a
Mateus Ceci <mateusceci@gmail.com>
2024-11-03 22:57:48 -0300

    fix: ensure carousel smooth

--- components/CarouselDays.tsx
+++ components/CarouselDays.tsx
@@ -34,72 +34,67 @@ const CarouselDays = ({ forecastData, location }: CarouselDaysProps) => {
   const width = Dimensions.get("window").width;
 
   return (
-    <View className="flex-1 mx-auto mt-16">
-      <Carousel
-        width={width - 20}
-        height={width / 1.5}
-        data={forecastData}
-        loop={false}
-        scrollAnimationDuration={500}
-        mode="parallax"
-        renderItem={({ item }) => (
-          <TouchableOpacity
-            className="flex-1 rounded-2xl bg-[rgba(255,255,255,0.4)] px-8 py-4 items-center"
-            onPress={() =>
-              navigation.navigate("Forecast", {
-                item,
-                title: location?.name,
-              })
-            }
-          >
-            <View className="items-center">
-              <Text className="text-4xl font-semibold">
-                {getDay(item?.date)}
-              </Text>
-              <Image
-                source={{ uri: `https:${item?.day?.condition?.icon}` }}
-                className="w-32 h-32"
-              />
-              <Text className="text-2xl font-semibold">
-                {item?.day?.condition?.text}
+    <Carousel
+      width={width - 20}
+      height={width / 1.5}
+      data={forecastData}
+      loop={false}
+      scrollAnimationDuration={1000}
+      style={{
+        marginVertical: 10,
+      }}
+      mode="parallax"
+      renderItem={({ item }) => (
+        <TouchableOpacity
+          className="flex-1 rounded-2xl bg-[rgba(255,255,255,0.4)] px-8 py-4 items-center"
+          onPress={() =>
+            navigation.navigate("Forecast", {
+              item,
+              title: location?.name,
+            })
+          }
+        >
+          <View className="items-center">
+            <Text className="text-4xl font-semibold">{getDay(item?.date)}</Text>
+            <Image
+              source={{ uri: `https:${item?.day?.condition?.icon}` }}
+              className="w-32 h-32"
+            />
+            <Text className="text-2xl font-semibold">
+              {item?.day?.condition?.text}
+            </Text>
+          </View>
+          <View className="flex-row justify-between w-full my-2">
+            <View>
+              <Entypo name="drop" size={32} color="white" />
+              <Text className="font-bold text-lg">
+                {item?.day?.avghumidity}%
               </Text>
             </View>
-            <View className="flex-row justify-between w-full my-2">
-              <View>
-                <Entypo name="drop" size={32} color="white" />
-                <Text className="font-bold text-lg">
-                  {item?.day?.avghumidity}%
-                </Text>
+            <View className="gap-2">
+              <View className="flex-row gap-2 items-center">
+                <Feather name="sun" size={24} color="black" />
+                <Text className="font-bold text-lg">{item?.astro.sunrise}</Text>
               </View>
-              <View className="gap-2">
-                <View className="flex-row gap-2 items-center">
-                  <Feather name="sun" size={24} color="black" />
-                  <Text className="font-bold text-lg">
-                    {item?.astro.sunrise}
-                  </Text>
-                </View>
-                <View className="flex-row gap-2 items-center">
-                  <Feather name="sunset" size={24} color="black" />
-                  <Text className="font-bold text-lg">
-                    {item?.astro.sunset}
-                  </Text>
-                </View>
-              </View>
-              <View className="gap-2">
-                <Text className="font-bold flex gap-2">
-                  <AntDesign name="arrowup" size={24} color="black" />
-                  <Text className="text-lg">{item?.day?.maxtemp_c}°C</Text>
-                </Text>
-                <Text className="font-bold flex gap-2">
-                  <AntDesign name="arrowdown" size={24} color="black" />
-                  <Text className="text-lg">{item?.day?.mintemp_c}°C</Text>
-                </Text>
+              <View className="flex-row gap-2 items-center">
+                <Feather name="sunset" size={24} color="black" />
+                <Text className="font-bold text-lg">{item?.astro.sunset}</Text>
               </View>
             </View>
-          </TouchableOpacity>
-        )}
-      />
-    </View>
+            <View className="gap-2">
+              <Text className="font-bold flex gap-2">
+                <AntDesign name="arrowup" size={24} color="black" />
+                <Text className="text-lg">{item?.day?.maxtemp_c}°C</Text>
+              </Text>
+              <Text className="font-bold flex gap-2">
+                <AntDesign name="arrowdown" size={24} color="black" />
+                <Text className="text-lg">{item?.day?.mintemp_c}°C</Text>
+              </Text>
+            </View>
+          </View>
+        </TouchableOpacity>
+      )}
+    />
   );
 };
 
