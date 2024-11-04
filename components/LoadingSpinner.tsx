f0189eb536e985fad04397468c62395078e3d247
Mateus Ceci <mateusceci@gmail.com>
2024-11-04 09:24:16 -0300

    feat: add height

--- components/LoadingSpinner.tsx
+++ components/LoadingSpinner.tsx
@@ -3,7 +3,7 @@ import { ActivityIndicator, View } from "react-native";
 
 const LoadingSpinner = () => {
   return (
-    <View className="flex-1 justify-center items-center">
+    <View className="flex-1 justify-center items-center h-96">
       <ActivityIndicator size="large" color="#0000ff" />
     </View>
   );
