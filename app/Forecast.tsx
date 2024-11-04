9ce7513fb3820cc44f11f3fefdf58a855945dd8f
Mateus Ceci <mateusceci@gmail.com>
2024-11-04 09:24:06 -0300

    feat: add type to item from route params

--- app/Forecast.tsx
+++ app/Forecast.tsx
@@ -2,9 +2,39 @@ import { getDay, getHour } from "@/lib/utils";
 import { useRoute } from "@react-navigation/native";
 import { LinearGradient } from "expo-linear-gradient";
 import { Text, View, Image, ScrollView } from "react-native";
+import { RouteProp } from "@react-navigation/native";
+
+type RouteParams = {
+  params: {
+    item: {
+      date: string;
+      day: {
+        condition: {
+          icon: string;
+          text: string;
+        };
+        maxtemp_c: number;
+        mintemp_c: number;
+        avghumidity: number;
+      };
+      astro: {
+        sunrise: string;
+        sunset: string;
+      };
+      hour: {
+        time: string;
+        condition: {
+          icon: string;
+          text: string;
+        };
+        temp_c: number;
+      }[];
+    };
+  };
+};
 
 const Forecast = () => {
-  const route = useRoute();
+  const route = useRoute<RouteProp<RouteParams>>();
   const { item } = route.params;
 
   return (
@@ -27,14 +57,14 @@ const Forecast = () => {
             <Text className="text-xl">Min Temp: {item?.day?.mintemp_c}°C</Text>
           </View>
           <View>
-            <Text className="text-lg">Sunrise: {item?.astro?.sunrise}</Text>
-            <Text className="text-lg">Sunset: {item?.astro?.sunset}</Text>
+            <Text className="text-xl">Sunrise: {item?.astro?.sunrise}</Text>
+            <Text className="text-xl">Sunset: {item?.astro?.sunset}</Text>
           </View>
         </View>
         <Text className="text-lg text-center mb-4">
           Humidity: {item?.day?.avghumidity}%
         </Text>
-        {item?.hour.map((hour, index) => (
+        {item?.hour.map((hour, index: number) => (
           <View
             className="flex-row justify-between items-center rounded-2xl p-2 mb-2 bg-[rgba(255,255,255,0.4)]"
             key={index}
